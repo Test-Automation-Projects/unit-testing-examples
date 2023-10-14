b3e3ccb68f3ad8ef4a495042411a8ceab4798e17
Alex Pshe <alexandra.psheborovskaya@jetbrains.com>
2023-10-14 10:45:41 +0600

    Functions 0

---
+++ src/main/kotlin/functions0.kt
@@ -0,0 +1,16 @@
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun munis(a: Int, b: Int): Int {
+    return a - b
+}
+
+fun multiple(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun devide(a: Int, b: Int): Int {
+    return a / b
+}
